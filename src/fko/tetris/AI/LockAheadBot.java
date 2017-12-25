300715f1d5526f6c9ec900208e6d254614a28eb0
Frank Kopp <frank@familie-kopp.de>
2017-12-25 15:36:57 +0100

    Working Bot

--- src/fko/tetris/AI/LockAheadBot.java
+++ src/fko/tetris/AI/LockAheadBot.java
@@ -3,51 +3,62 @@ package fko.tetris.AI;
 import java.util.ArrayList;
 import java.util.List;
 
-import fko.tetris.game.Playfield;
+import fko.tetris.game.Matrix;
 import fko.tetris.game.TetrisColor;
 import fko.tetris.game.TetrisControlEvents;
 import fko.tetris.game.TetrisGame;
 import fko.tetris.game.TetrisPhase;
 import fko.tetris.tetriminos.Tetrimino;
+import fko.tetris.util.HelperTools;
 
 
 public class LockAheadBot extends AbstractBot {
-	
-	private static final int MAX_VISIBLE_NEXTQUEUE = 1;
-	
+
+	private static final int MAX_VISIBLE_NEXTQUEUE = 2;
+
 	private List<Tetrimino> _nextQueue = new ArrayList<Tetrimino>(); 
-	
+
 	private int _numberOfEvaluations = 0;
 
+	/**
+	 * Creates a bot with a handle to the game
+	 * @param game
+	 */
 	public LockAheadBot(TetrisGame game) {
 		super(game);
 		System.out.println("MINIMAX BOT CREATED");
 	}
 
+	/**
+	 * run the bot as long as Thread is not interrupted. 
+	 */
 	@Override
 	public void run() {
 		System.out.println("MINIMAX BOT STARTED");
+		
 		while (!Thread.interrupted()) {
 			try {
 
 				final TetrisPhase phaseState = _game.getPhaseState();
-				
+
 				switch(phaseState) {
 				case FALLING: {
+					long time = System.currentTimeMillis();
 					_numberOfEvaluations = 0;
 					_nextQueue.clear();
 					for (int i=0; i<=MAX_VISIBLE_NEXTQUEUE; i++) {
 						_nextQueue.add(_game.getNextQueue().get(i));
 					}
 					placeTetrimino();
+					System.out.println("Bot took "+HelperTools.formatTime(System.currentTimeMillis()-time, true));
 					break;
 				}
 				case GAMEOVER: Thread.currentThread().interrupt(); break;
 				default: break;
 				}
-				
+
 				// slow down a bit
-				Thread.sleep(100);
+				Thread.sleep(0);
 
 			} catch (InterruptedException e) {
 				break;
@@ -67,7 +78,7 @@ public class LockAheadBot extends AbstractBot {
 
 		// for each permutation of move position and turn position get a score
 		for (int turn=0; turn<4; turn++) {
-			Playfield pf = _game.getPlayfield().clone();
+			Matrix pf = _game.getPlayfield().clone();
 			for (int i=0; i<turn; i++) pf.turnMove(1); // make the turn
 			// determine max moves right and left
 			int moveR=0;
@@ -80,7 +91,7 @@ public class LockAheadBot extends AbstractBot {
 			}
 			// now we are on the left - for each position horizontally make the drop a call recursive function
 			for (int m = moveL; m <= moveR; m++) {
-				Playfield pfm = pf.clone();
+				Matrix pfm = pf.clone();
 				// move right
 				for (int i=0; i<m-moveL; i++) {
 					pfm.moveSideway(1);
@@ -93,7 +104,7 @@ public class LockAheadBot extends AbstractBot {
 					return; // game over
 				}
 				int score = bruteForceTree(pfm, 1);
-				
+
 				if (score > best_score) {
 					best_turn = turn;
 					best_move = m;
@@ -106,7 +117,7 @@ public class LockAheadBot extends AbstractBot {
 		for (int i=0; i<best_turn; i++) {
 			_game.controlQueueAdd(TetrisControlEvents.RTURN);
 		}
-		
+
 		// now move to the best position
 		for (int i=0; i < Math.abs(best_move); i++) {
 			if (best_move < 0) {
@@ -116,24 +127,27 @@ public class LockAheadBot extends AbstractBot {
 			}
 		}
 		
-		// finally a hard drop
-		System.out.println("SCORE: "+best_score);
-		System.out.println(String.format("Evaluations: %,d",_numberOfEvaluations));
-		System.out.println("BOT MAKES MOVE");
 		_game.controlQueueAdd(TetrisControlEvents.HARDDOWN);
+
+		// finally a hard drop
+//		System.out.println("BEST TURN: "+best_turn+" BEST MOVE: "+best_move);
+//		System.out.println("BEST SCORE: "+best_score);
+		System.out.println(String.format("Evaluations: %,d",_numberOfEvaluations));
+		System.out.println(">>>>>>>>>>>>>>>>>>>> BOT MAKES MOVE <<<<<<<<<<<<<<<<<<<<<<<<<<");
+		
 	}
 
-	private int bruteForceTree(Playfield playfield, int nextQueueIndex) {
+	private int bruteForceTree(Matrix playfield, int nextQueueIndex) {
 		if (Thread.currentThread().isInterrupted()) return Integer.MIN_VALUE;
-		
+
 		if (nextQueueIndex >= MAX_VISIBLE_NEXTQUEUE) {
 			return evalutation(playfield);
 		}
-		
+
 		int best_score = Integer.MIN_VALUE;
-		
+
 		for (int turn=0; turn<4; turn++) {
-			Playfield pf = playfield.clone();
+			Matrix pf = playfield.clone();
 			for (int i=0; i<turn; i++) pf.turnMove(1); // make the turn
 			// determine max moves right and left
 			int moveR=0;
@@ -146,7 +160,7 @@ public class LockAheadBot extends AbstractBot {
 			}
 			// now we are on the left - for each position horizontally make the drop a call recursive function
 			for (int m = moveL; m <= moveR; m++) {
-				Playfield pfm = pf.clone();
+				Matrix pfm = pf.clone();
 				// move right
 				for (int i=0; i<m-moveL; i++) {
 					pfm.moveSideway(1);
@@ -167,63 +181,93 @@ public class LockAheadBot extends AbstractBot {
 		}
 		return best_score;
 	}
-	
-	private int evalutation(Playfield pf) {
-		
+
+	private int evalutation(Matrix pf) {
+
 		_numberOfEvaluations++;
-		
+
 		if (_numberOfEvaluations%100000 == 0) {
 			System.out.println(String.format("%,d",_numberOfEvaluations));
 		}
-		
+
 		int score = 0;
-		
+
 		final double weightabsolutHeight 	= -1.0;	// Absolute height - higher is worse
 		final double weightAggregatedHeight 	= -1.0;	// Aggregated height - higher is worse
-		final double weightUnevenness 		= -0.2;	// unevenness of stacks - higher is worse
-		
+		final double weightUnevenness 		= -1.0;	// unevenness of stacks - higher is worse
+		final double weightHoles				= -2.0;	// holes - higher is worse
+		final double weightBlocker			= -3.0;	// minos blocking a hole - higher is worse
+
 		//pf.debugPrintMatrix();
-		
+
 		int [] result = scanFieldEvaluations(pf);
-		
-		score += weightabsolutHeight		* (result[0]^2); // more weight to height if higher 
+
+		score += weightabsolutHeight		* result[0]; // more weight to height if higher 
 		score += weightAggregatedHeight	* result[1];
 		score += weightUnevenness		* result[2];
-		
+		score += weightHoles				* result[3];
+		score += weightBlocker			* result[4];
+
 //		System.out.println("SCORE: "+score);
-		
+
 		return score;
 	}
 
-	
+
 	/*
 	 * scans the whole field and calculates various evaluations and returns the as an array
-	 * { absolute height, aggregated height }
+	 * { absolute height, aggregated height, unevenness, holes, unwanted blocker }
 	 */
-	private int[] scanFieldEvaluations(Playfield pf) {
+	private int[] scanFieldEvaluations(Matrix pf) {
 		int aggregatedHeight = 0;
 		int absoluteHeight = 0;
 		int unevennessLast = 0;
 		int unevenness = 0;
-		for (int x=0; x<Playfield.PLAYFIELD_WIDTH;x++) {
+		int holes = 0;
+		int blocker = 0;
+
+		// scan the field once and do all evaluations which need to scan the whole matrix
+		// to avoid scanning the matrix multiple times
+		for (int x=0; x<Matrix.PLAYFIELD_WIDTH;x++) {
 			int lastHeigth = 0;
-			for (int y=0; y<Playfield.PLAYFIELD_HEIGHT; y++) {
+			int possibleHoles = 0;
+			int tmpHoles = 0;
+			int tmpBlocker = 0;
+			for (int y=0; y<Matrix.PLAYFIELD_HEIGHT; y++) {
 				if (pf.getCell(x, y) != TetrisColor.EMPTY) {
 					lastHeigth = y+1; 
+					if (possibleHoles > 0) {
+						tmpHoles += possibleHoles;
+						possibleHoles = 0;
+						tmpBlocker++;
+					}
+				} else {
+					possibleHoles++;
 				}
+			}
+			if (tmpHoles > 0 && tmpBlocker > 0) {
+				holes += tmpHoles;
+				blocker += tmpBlocker;
 			}
 			aggregatedHeight += lastHeigth;
 			unevenness += Math.abs(unevennessLast-lastHeigth);
 			unevennessLast = lastHeigth;
 			if (lastHeigth > absoluteHeight) absoluteHeight = lastHeigth;
 		}
-		
+
 //		pf.debugPrintMatrix();
 //		System.out.println("AGGREGATED HEIGHT: "+aggregatedHeight);
 //		System.out.println("ABSOLTUE HEIGHT: "+absoluteHeight);
 //		System.out.println("UNEVENNESS: "+unevenness);
-		
-		return new int[] {aggregatedHeight, absoluteHeight*absoluteHeight, unevenness};
+//		System.out.println("HOLES: "+holes);
+//		System.out.println("BLOCKER: "+blocker);
+
+		return new int[] {
+				aggregatedHeight, 
+				absoluteHeight, // to emphasize height 
+				unevenness,
+				holes,
+				blocker};
 	}
 
 }
