15c3df378ec4b14b8750232a7bfae2c18f252c4a
Frank Kopp <frank@familie-kopp.de>
2018-01-22 18:30:57 +0100

    AI NN Testing

--- src/main/java/fko/tetris/AI/NeuralNetworkBotTrainer.java
+++ src/main/java/fko/tetris/AI/NeuralNetworkBotTrainer.java
@@ -41,9 +41,13 @@ public class NeuralNetworkBotTrainer {
 
   private static final Logger LOG = LoggerFactory.getLogger(NeuralNetworkBotTrainer.class);
 
+  // paths to training data
   private static final String folderPathPlain = "./var/";
   private static final String fileNamePlainTrain = "trainingdata_train.csv";
-  private static final String fileNamePlainTest  = "trainingdata_test.csv";
+  private static final String fileNamePlainTest = "trainingdata_test.csv";
+
+  // where to store the trained network
+  public static final String NN_SAVE_FILE = folderPathPlain + "tetris_nn_model.zip";
 
   /**
    * Run this to train the NeuralNetworkBot and save the Network to file.
@@ -66,13 +70,11 @@ public class NeuralNetworkBotTrainer {
     uiServer.attach(statsStorage);
 
     // Configuration
-    int height      = 26;   // 22 matrix, +2 current tetrimino, +2 next
-    int width       = 10;
-    int channels    = 1;
-    int outputNum   = 44;
-    int batchSize   = 100;
-    int nEpochs     = 2;
-    int iterations  = 2;
+    int height = 26; // 22 matrix, +2 current tetrimino, +2 next
+    int width = 10; // tetris is 10 blocks wide
+    int channels = 1; // we only need 1 color (black & white) - color has no real meaning in tetris
+    int outputNum = 44; // 4 turns and 11 moves (-5, 0, +5)
+    int batchSize = 64;
 
     int seed = 1234;
 
@@ -86,90 +88,96 @@ public class NeuralNetworkBotTrainer {
     LOG.info("Reading train file {}{}", folderPathPlain, fileNamePlainTrain);
     RecordReader trainDataCSVReader = new CSVRecordReader(numLinesToSkip, delimiter);
     trainDataCSVReader.initialize(new FileSplit(new File(folderPathPlain + fileNamePlainTrain)));
-    DataSetIterator trainIter = new RecordReaderDataSetIterator(trainDataCSVReader, batchSize, height*width, 44);
+    DataSetIterator trainIter =
+        new RecordReaderDataSetIterator(trainDataCSVReader, batchSize, height * width, 44);
 
     LOG.info("Reading test file {}{}", folderPathPlain, fileNamePlainTest);
     RecordReader testDataCSVReader = new CSVRecordReader(numLinesToSkip, delimiter);
     testDataCSVReader.initialize(new FileSplit(new File(folderPathPlain + fileNamePlainTest)));
-    DataSetIterator testIter = new RecordReaderDataSetIterator(testDataCSVReader, batchSize, height*width, 44);
+    DataSetIterator testIter =
+        new RecordReaderDataSetIterator(testDataCSVReader, batchSize, height * width, 44);
+
+    // Configuration
+    int nEpochs = 8;
+    int iterations = 4;
+    final WeightInit weightInit = WeightInit.XAVIER;
+    final OptimizationAlgorithm optimizationAlgo = OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT;
+    final Updater updater = Updater.NESTEROVS;
+    final LossFunctions.LossFunction lossFunction = LossFunctions.LossFunction.NEGATIVELOGLIKELIHOOD;
 
     LOG.info("Build model....");
     Map<Integer, Double> lrSchedule = new HashMap<>();
-    lrSchedule.put(0,    0.1); // iteration #, learning rate
-    lrSchedule.put(200,  0.07);
-    lrSchedule.put(600,  0.03);
-    lrSchedule.put(800,  0.01);
+    lrSchedule.put(0, 0.08); // iteration #, learning rate
+    lrSchedule.put(200, 0.05);
+    lrSchedule.put(600, 0.03);
+    lrSchedule.put(800, 0.01);
     lrSchedule.put(1000, 0.005);
+    lrSchedule.put(1500, 0.001);
 
     MultiLayerConfiguration conf =
-            new NeuralNetConfiguration.Builder()
-                    .seed(seed)
-                    .iterations(iterations)
-                    .miniBatch(true)
-                    .regularization(true)
-                    .l2(0.0005)
-                    .learningRate(.01)
-                    .learningRateDecayPolicy(LearningRatePolicy.Schedule)
-                    .learningRateSchedule(lrSchedule) // overrides the rate set in learningRate
-                    .weightInit(WeightInit.XAVIER)
-                    .optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT)
-                    .updater(Updater.NESTEROVS)
-                    .list()
-
-                    .layer(
-                            0,
-                            new ConvolutionLayer.Builder(4, 4)
-                                    .nIn(channels)
-                                    .stride(1, 1)
-                                    .nOut(20)
-                                    .activation(Activation.IDENTITY)
-                                    .build())
-                    .layer(
-                            1,
-                            new SubsamplingLayer.Builder(SubsamplingLayer.PoolingType.MAX)
-                                    .kernelSize(2, 2)
-                                    .stride(1, 1)
-                                    .build())
-                    .layer(
-                            2,
-                            new ConvolutionLayer.Builder(4, 4)
-//                                    .nIn(100)
-                                    .stride(1, 1) // nIn need not specified in later layers
-                                    .nOut(50)
-                                    .activation(Activation.IDENTITY)
-                                    .build())
-                    .layer(
-                            3,
-                            new SubsamplingLayer.Builder(SubsamplingLayer.PoolingType.MAX)
-                                    .kernelSize(2, 2)
-                                    .stride(2, 2)
-                                    .build())
-                    .layer(
-                            4,
-                            new DenseLayer.Builder()
-//                                    .nIn(200)
-                                    .activation(Activation.RELU)
-                                    .nOut(500)
-                                    .build())
-                    .layer(
-                            5,
-                            new OutputLayer.Builder(LossFunctions.LossFunction.NEGATIVELOGLIKELIHOOD)
-//                                    .nIn(400)
-                                    .nOut(outputNum)
-                                    .activation(Activation.SOFTMAX)
-                                    .build())
-
-                    .setInputType(InputType.convolutionalFlat(height, width, channels))
-
-                    .backprop(true)
-                    .pretrain(false)
-                    .build();
+        new NeuralNetConfiguration.Builder()
+            .seed(seed)
+            .iterations(iterations)
+            .miniBatch(true)
+            .regularization(true)
+            .l2(0.0005)
+            .learningRate(0.1)
+            .learningRateDecayPolicy(LearningRatePolicy.Schedule)
+            .learningRateSchedule(lrSchedule) // overrides the rate set in learningRate
+            .weightInit(weightInit)
+            .optimizationAlgo(optimizationAlgo)
+            .updater(updater)
+            .list()
+            .layer(
+                0,
+                new ConvolutionLayer.Builder(2, 2)
+                    .nIn(channels)
+                    .stride(1, 1)
+                    .padding(1, 1)
+                    .nOut(40)
+                    .activation(Activation.IDENTITY)
+                    .build())
+            .layer(
+                1,
+                new SubsamplingLayer.Builder(SubsamplingLayer.PoolingType.MAX)
+                    .kernelSize(2, 2)
+                    .stride(1, 1)
+                    .build())
+            .layer(
+                2,
+                new ConvolutionLayer.Builder(4, 4)
+                    .stride(1, 1) // nIn need not specified in later layers
+                    .nOut(80)
+                    .activation(Activation.IDENTITY)
+                    .build())
+            .layer(
+                3,
+                new SubsamplingLayer.Builder(SubsamplingLayer.PoolingType.MAX)
+                    .kernelSize(2, 2)
+                    .stride(2, 2)
+                    .build())
+            .layer(
+                4,
+                new DenseLayer.Builder()
+                    .activation(Activation.RELU)
+                    .nOut(320)
+                    .build())
+            .layer(
+                5,
+                new OutputLayer.Builder(lossFunction)
+                    .nOut(outputNum)
+                    .activation(Activation.SOFTMAX)
+                    .build())
+            .setInputType(InputType.convolutionalFlat(height, width, channels))
+            .backprop(true)
+            .pretrain(false)
+            .build();
 
     LOG.debug("Model configured");
 
     MultiLayerNetwork net = new MultiLayerNetwork(conf);
     net.init();
-    net.setListeners(new ScoreIterationListener(100)); // console
+    net.setListeners(new ScoreIterationListener(50)); // console
     net.addListeners(new StatsListener(statsStorage)); // web ui
 
     LOG.debug("Total num of params: {}", net.numParams());
@@ -181,14 +189,14 @@ public class NeuralNetworkBotTrainer {
         trainIter.next();
         d.getAndAdd(trainIter.batch());
       }
-      LOG.debug("Number of Train examples {}",d);
+      LOG.debug("Number of Train examples {}", d);
 
       d.set(0);
       while (testIter.hasNext()) {
         testIter.next();
         d.getAndAdd(testIter.batch());
       }
-      LOG.debug("Number of Test examples {}",d);
+      LOG.debug("Number of Test examples {}", d);
 
       trainIter.reset();
       testIter.reset();
@@ -196,7 +204,7 @@ public class NeuralNetworkBotTrainer {
 
     // evaluation while training (the score should go down)
     for (int i = 0; i < nEpochs; i++) {
-      LOG.info("Starting epoch {}", i+1);
+      LOG.info("Starting epoch {}", i + 1);
       LOG.info("Starting training");
       net.fit(trainIter);
       LOG.info("Starting evaluating");
@@ -204,12 +212,11 @@ public class NeuralNetworkBotTrainer {
       LOG.info(eval.stats());
       trainIter.reset();
       testIter.reset();
-      LOG.info("Completed epoch {}", i+1);
+      LOG.info("Completed epoch {}", i + 1);
     }
 
-    String saveFile = folderPathPlain + "/tetris_nn_model.zip";
-    LOG.info("Finished training. Writing model to file {}", saveFile);
-    ModelSerializer.writeModel(net, new File(saveFile), true);
+    LOG.info("Finished training. Writing model to file {}", NN_SAVE_FILE);
+    ModelSerializer.writeModel(net, new File(NN_SAVE_FILE), true);
 
     System.exit(0);
   }
