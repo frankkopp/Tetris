d91e69534f12d960ebffb0295f3d46c84268d1b2
Frank Kopp <frank@familie-kopp.de>
2018-01-28 23:41:40 +0100

    Is able to use reduced input dataset now - zooming to a few lines instead whole field

--- src/main/java/fko/tetris/AI/NeuralNetworkBot.java
+++ src/main/java/fko/tetris/AI/NeuralNetworkBot.java
@@ -1,7 +1,9 @@
 package fko.tetris.AI;
 
+import com.google.common.collect.Lists;
 import fko.tetris.game.*;
 import fko.tetris.tetriminos.Tetrimino;
+import org.apache.commons.lang.ArrayUtils;
 import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
 import org.deeplearning4j.util.ModelSerializer;
 import org.nd4j.linalg.api.ndarray.INDArray;
@@ -12,7 +14,9 @@ import org.slf4j.LoggerFactory;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * This implementation creates a NeuraNetwork driven Bot.<br>
@@ -24,13 +28,15 @@ public class NeuralNetworkBot extends AbstractBot {
 
   private static final int MAX_VISIBLE_NEXTQUEUE = 2;
 
-  private final List<Tetrimino> _nextQueue = new ArrayList<Tetrimino>();
+  // reduction of the input from the position to only have this number of relevant lines plus the 4 lines for
+  // current and next tetrimino
+  private static final int NUMBER_OF_ZOOM_LINES = 5;
+
+  private final List<Tetrimino> _nextQueue = new ArrayList<>();
 
   /* default value for folder */
   private static final String folderPathPlain = "./var/";
-  private static final String fileNamePlain = "tetris_nn_model_F1-05132.zip";
-  private static final String fileNamePlainTrain = "trainingdata_train.csv";
-  private static final String fileNamePlainTest = "trainingdata_test.csv";
+  private static final String fileNamePlain = "tetris_nn_model_zoom5.zip";
 
   private MultiLayerNetwork botAI = null;
 
@@ -52,34 +58,35 @@ public class NeuralNetworkBot extends AbstractBot {
     }
   }
 
-  /** Run the bot as long as Thread is not interrupted. */
+  /**
+   * Run the bot as long as Thread is not interrupted.
+   */
   @Override
   public void run() {
     boolean moveDone = false; // to prevent several calculations during the falling phase
     while (!Thread.interrupted()) {
       final TetrisPhase phaseState = game.getPhaseState();
       switch (phaseState) {
-          // we can only move when we are in FALLING phase
+        // we can only move when we are in FALLING phase
         case LOCK: // we can still move during LOCK - this is helpful in higher levels when game is
           // really fast
-        case FALLING:
-          {
-            if (!moveDone) {
-              long time = System.nanoTime();
-              _nextQueue.clear();
-              // copy the nextQueue into a list
-              for (int i = 0; i <= MAX_VISIBLE_NEXTQUEUE; i++) {
-                _nextQueue.add(game.getNextQueue().get(i));
-              }
-              // calculate the best position and place Tetrimino
-              placeTetrimino();
-              moveDone = true;
-              System.out.println(String.format("Bot took %,10d ns", (System.nanoTime() - time)));
-              System.out.println();
+        case FALLING: {
+          if (!moveDone) {
+            long time = System.nanoTime();
+            _nextQueue.clear();
+            // copy the nextQueue into a list
+            for (int i = 0; i <= MAX_VISIBLE_NEXTQUEUE; i++) {
+              _nextQueue.add(game.getNextQueue().get(i));
             }
-            break;
+            // calculate the best position and place Tetrimino
+            placeTetrimino();
+            moveDone = true;
+            System.out.println(String.format("Bot took %,10d ns", (System.nanoTime() - time)));
+            System.out.println();
           }
-          // game over stops thread
+          break;
+        }
+        // game over stops thread
         case GAMEOVER:
           moveDone = false;
           Thread.currentThread().interrupt();
@@ -119,11 +126,11 @@ public class NeuralNetworkBot extends AbstractBot {
 
     // Translate from NN output
 
-    best_turn = argMax/11;
-    System.out.println("Turn: " + best_turn);
+    best_turn = argMax / 11;
+    LOG.info("Turn: {}", best_turn);
 
-    best_move = (argMax%11) -5;
-    System.out.println("Move: " + best_move);
+    best_move = (argMax % 11) - 5;
+    LOG.info("Move: {}", best_move);
 
     // now turn to the best position on the real matrix
     for (int i = 0; i < best_turn; i++) {
@@ -154,7 +161,7 @@ public class NeuralNetworkBot extends AbstractBot {
 
     // 22x10 + 2x10 current + 2x10 next tetrimino
     int matrixWidth = Matrix.MATRIX_WIDTH;
-    float [] flatNormalizedMatrix = new float[(Matrix.MATRIX_HEIGHT+4)* matrixWidth];
+    float[] flatNormalizedMatrix = new float[(Matrix.MATRIX_HEIGHT + 4) * matrixWidth];
 
     int[][] tMatrix;
 
@@ -165,9 +172,9 @@ public class NeuralNetworkBot extends AbstractBot {
     for (int y = 0; y < 2; y++) {
       for (int x = 0; x < matrixWidth; x++) {
         if (x < nextTetrimino.getCurrentPosition().x || x >= nextTetrimino.getCurrentPosition().x + tMatrix[y].length) {
-          flatNormalizedMatrix[y*matrixWidth+x] = 0f;
+          flatNormalizedMatrix[y * matrixWidth + x] = 0f;
         } else {
-          flatNormalizedMatrix[y*matrixWidth+x] = tMatrix[y][x - nextTetrimino.getCurrentPosition().x] == 0 ? 0f : 1f;
+          flatNormalizedMatrix[y * matrixWidth + x] = tMatrix[y][x - nextTetrimino.getCurrentPosition().x] == 0 ? 0f : 1f;
         }
       }
     }
@@ -179,9 +186,9 @@ public class NeuralNetworkBot extends AbstractBot {
     for (int y = 0; y < 2; y++) {
       for (int x = 0; x < matrixWidth; x++) {
         if (x < currentTetrimino.getCurrentPosition().x || x >= currentTetrimino.getCurrentPosition().x + tMatrix[y].length) {
-          flatNormalizedMatrix[(y+2)*matrixWidth+x] = 0f;
+          flatNormalizedMatrix[(y + 2) * matrixWidth + x] = 0f;
         } else {
-          flatNormalizedMatrix[(y+2)*matrixWidth+x] = tMatrix[y][x - currentTetrimino.getCurrentPosition().x] == 0 ? 0f : 1f;
+          flatNormalizedMatrix[(y + 2) * matrixWidth + x] = tMatrix[y][x - currentTetrimino.getCurrentPosition().x] == 0 ? 0f : 1f;
         }
       }
     }
@@ -189,24 +196,48 @@ public class NeuralNetworkBot extends AbstractBot {
     // flatten matrix and normalize everything to 0 and 1
     for (int y = 0; y < Matrix.MATRIX_HEIGHT; y++) {
       for (int x = 0; x < matrixWidth; x++) {
-
-        flatNormalizedMatrix[(Matrix.MATRIX_HEIGHT-1-y+4)*matrixWidth+x] =
+        flatNormalizedMatrix[(Matrix.MATRIX_HEIGHT - 1 - y + 4) * matrixWidth + x] =
                 matrix.getCell(x, y) == TetrisColor.EMPTY ? 0f : 1f;
-
       }
     }
 
-    if (LOG.isTraceEnabled()) {
-      String line = System.lineSeparator();
-      for (int i=0; i<flatNormalizedMatrix.length; i++) {
-        line += (int) flatNormalizedMatrix[i] + " ";
-        if ((i+1) % 10 == 0) {
-          line += System.lineSeparator();
-        }
+    return new NDArray(getReducedMatrix(flatNormalizedMatrix, NUMBER_OF_ZOOM_LINES));
+  }
+
+  private float[] getReducedMatrix(final float[] flatNormalizedMatrix, int nZoomLines) {
+
+    List<Float> newList = new ArrayList<>();
+
+    int nRows = 0;
+    int i = 0;
+    boolean foundNonZero = false;
+    List<Float> tmp = new ArrayList<>();
+
+    for (float d : flatNormalizedMatrix) {
+      tmp.add(d);
+      i++;
+      if (d != 0d) foundNonZero = true;
+      if (i % 10 == 0) { // new row every 10 items
+        // first 4 rows always
+        if (nRows < 4) {
+          foundNonZero = false;
+          newList.addAll(tmp);
+          nRows++;
+        } else
+          // remove empty (zero) lines but leave at least nZoomLines rows left
+          if ((nRows < 4 + nZoomLines
+                  // when non zero items in line and we do not have enough lines yet
+                  && (foundNonZero
+                  // at least as many rows as need as per nZoomLines
+                  || ((26 - i / 10) < nRows - 4 + nZoomLines) && nRows < 4 + nZoomLines))) {
+            newList.addAll(tmp);
+            nRows++;
+          }
+        tmp.clear();
       }
-      LOG.trace(line);
     }
 
-    return new NDArray(flatNormalizedMatrix);
+    return ArrayUtils.toPrimitive(newList.toArray(new Float[0]),0.0F);
+
   }
 }
